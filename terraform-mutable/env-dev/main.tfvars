502d9add83c45e0cc18297324f639221e79fd9ca
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-05 23:15:44 +0530

    Update roboshop-components/cart/

--- terraform-mutable/env-dev/main.tfvars
+++ terraform-mutable/env-dev/main.tfvars
@@ -1,1 +1,4 @@
-ENV = "dev"
+ENV                 = "dev"
+INSTANCE_TYPE       = "t3.micro"
+SPOT_INSTANCE_COUNT = 1
+OD_INSTANCE_COUNT   = 0
--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,3 +1,5 @@
 module "cart" {
-  source = "git::https://github.com/roboshop-blue-green/tf-app-mutable-module.git"
+  source    = "git::https://github.com/roboshop-blue-green/tf-app-mutable-module.git"
+  COMPONENT = var.COMPONENT
+  APP_PORT  = var.APP_PORT
 }
