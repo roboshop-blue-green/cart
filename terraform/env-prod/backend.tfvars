dd2b079ad0e34788bdc8df9d398062c857f4ddf9
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-05 22:40:04 +0530

    Update roboshop-components/cart/

---
+++ terraform/env-prod/backend.tfvars
@@ -0,0 +1,4 @@
+bucket = "roboshop-bg"
+key    = "cart/prod/terraform.tfstate"
+region = "us-east-1"
+
---
+++ terraform/main.tf
@@ -0,0 +1,3 @@
+module "cart" {
+  source = "git::https://github.com/roboshop-blue-green/tf-app-immutable-module.git"
+}
